dc8dc2de93ffe0feb1dba9e6b7b9d5d24921312e
Bryce Thompson <bryceedthompson@gmail.com>
2023-05-08 16:07:02 -0700

    finished js and readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,3 @@
-
-
-
 //var saveButton = document.querySelector(".saveBtn");
 //var noteSaveArray = [];
 //var userNotes = document.querySelector(".saveBtn");
@@ -22,6 +19,9 @@ timeBlock.children().children("i").on("click" , (event)=>{
   var notes = save.parent().parent().children("textarea").val();
   var time = save.parent().parent().prop("id");
   localStorage.setItem(time,notes);
+  console.log(save);
+console.log(notes);
+console.log(time);
 });
 //saveButton.addEventListener("click",(event)=>{
   //event.preventDefault();
@@ -35,13 +35,33 @@ timeBlock.children().children("i").on("click" , (event)=>{
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  function applyTime(){
+    var currentTime = dayjs().format("H");
+    for (var i = 7; i < 18; i ++){
+      var timeId = "#hour-"+i;
+      if(currentTime == i){
+        $(timeId).removeClass("past future");
+        $(timeId).addClass("present");
+      }
+      else if(currentTime > i){
+        $(timeId).removeClass("present past");
+        $(timeId).addClass("future");
+      } else{
+        $(timeId).removeClass("future present");
+        $(timeId).addClass("past");
+      }
+    }
+  }
+  applyTime();
+setInterval(()=>{
+  applyTime();
+  },1000);
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  for (var i = 3; i < 18; i ++){
+  for (var i = 7; i < 18; i ++){
     var timeId = "#hour-"+i;
-    var saveId = "#hour-"+i;
+    var saveId = "hour-"+i;
     $(timeId).children("textarea").text(localStorage.getItem(saveId));
   }
 
@@ -49,6 +69,4 @@ timeBlock.children().children("i").on("click" , (event)=>{
   // TODO: Add code to display the current date in the header of the page.
   var currentDay = $('#currentDay');
   currentDay.text(dayjs().format('dddd, MMMM DD[th]'));// ask about current day formating 
-
-
 });
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # thirdparty-challenge
 third party API challenge
+# 
+# <third party API challenge>
+## Description
+
+This is a challenge that has me working with third party API's. For this challenge we were provided with the website built and a few things for us to finish. Some of the things that I fixed are:
+- When someone opens the planner the current day is displayed at the top of the calender 
+- When someone scrolls down they are presented twith a timeblock for some business hours 
+- When someone views the timeblocks they are color cordinated for past, present, and future times
+- When someone clicks on a timeblock they can type in somenotes and they are saved  in local storagewhen the page is reloaded
+- When the password is generated it is displayed on the page
+## Installation
+
+Open the link in my GitHub Repo!
+
+## Usage
+- The date and time color coding is updated on its own
+
+- You can click on the timeblocks to write notes for each block 
+
+- Clicking on the save button for each timeblock will save the notes even when the page is reloaded
+
+- Thats it!
+
+
+## Credits
+
+- https://developer.mozilla.org/en-US/docs/Web/API/setInterval
+
+- https://day.js.org/docs/en/display/format
+
+- https://jqueryui.com/demos/
+
+- https://api.jquery.com/category/traversing/
+
+- https://learn.jquery.com/events/event-delegation/
